535f9f9f50a160d235382b1c4b97d6a6edbc2d3d
森田浩平 <morita.aislab@gmail.com>
2016-12-06 11:54:54 +0900

    Depth画像をPointCloudに変換する処理をクラス化したのでコミット

--- PointCloudStudy/PointCloudStudy/FileConvert.cpp
+++ PointCloudStudy/PointCloudStudy/FileConvert.cpp
@@ -27,3 +27,27 @@ void FileConvert::convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXY
 
 	return ;
 }
+
+// DIND‚ÅŽæ“¾‚µ‚½Depth Image‚ðPoint Cloud‚É•ÏŠ·‚·‚é
+void convPNG2PointCloud(std::string filename, cv::Point param_fl, cv::Point param_pp){
+	for (int i = 0; i < 61; i++){
+		cv::Mat depthmap = cv::imread("Data_20161121/dataset_png/depth" + std::to_string(i) + ".png", -1);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+		for (int v = 0; v < depthmap.rows; v++){
+			for (int u = 0; u < depthmap.cols; u++){
+				pcl::PointXYZ point;
+
+				point.z = depthmap.at<UINT16>(v, u);
+				point.x = (u - param_pp.x) * point.z / param_fl.x;
+				point.y = (v - param_pp.y) * point.z / param_fl.y;
+
+				pointcloud->push_back(point);
+			}
+		}
+
+		pcl::io::savePLYFile("Data_20161121/dataset_pc/morichang" + std::to_string(i) + ".ply", *pointcloud);
+		pointcloud->clear();
+	}
+	return;
+}
--- PointCloudStudy/PointCloudStudy/FileConvert.h
+++ PointCloudStudy/PointCloudStudy/FileConvert.h
@@ -4,9 +4,16 @@
 
 
 class FileConvert{
-public:
+private:
+	
+	cv::Point focalLength;
+	cv::Point principalPoint;
 
+
+public:
+	
 	void convPCD2PLY(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud);
 	void convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud);
+	void convPNG2PointCloud(std::string filename, cv::Point param_fl, cv::Point param_pp);
 
 };
