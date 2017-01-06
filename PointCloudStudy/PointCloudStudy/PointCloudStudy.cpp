35206c2ebd7b067091e0964c377faf5197882f57
森田浩平 <morita.aislab@gmail.com>
2017-01-07 06:15:28 +0900

    Gitignoreで弾くファイル増やした&Reconstructしたポリゴンのフレーム切り替えて表示したいけどなんかできなかったので後で直す

--- PointCloudStudy/PointCloudStudy/PointCloudStudy.cpp
+++ PointCloudStudy/PointCloudStudy/PointCloudStudy.cpp
@@ -33,53 +33,44 @@ void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, 	void
 int _tmain(int argc, _TCHAR* argv[])
 {
 	char key = 0;
+	int i = 1;
 
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-	//pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud2(new pcl::PointCloud<pcl::PointXYZRGB>);
-	//pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud3(new pcl::PointCloud<pcl::PointXYZRGB>);
-	//pcl::PointCloud<pcl::PointXYZRGB>::Ptr merged_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-	//pcl::PointCloud<pcl::PointXYZRGB> merge;
-	//pcl::TextureMesh::Ptr textureMesh(new pcl::TextureMesh());
+	std::string name = "Data_20161121/dataset_obj/obj_frame";
+
+	cv::imshow("Key Capture", NULL);
+
 	pcl::PolygonMesh::Ptr polygonMesh(new pcl::PolygonMesh());
-	FileConvert conv;
-	//std::string name = "20151203T222732.959635_1";
-	//std::string name2 = "20151203T222733.621941_1";
-	//std::string name3 = "20151203T222734.016625_1";
-	std::string name = "20151203T222732";
 
-
-	//conv.convPCD2PLY(name, cloud);
-	
-	//pcl::io::loadPCDFile(name + ".pcd", *cloud);
-	//pcl::io::loadPLYFile(name + ".ply", *cloud);
-	//pcl::io::loadPLYFile(name2 + ".ply", *cloud2); 
-	//pcl::io::loadPLYFile(name3 + ".ply", *cloud3);
-	//pcl::io::loadOBJFile(name + ".obj", *polygonMesh);
-	//pcl::io::loadOBJFile("merged_mesh.obj", *polygonMesh);
-	pcl::io::loadPLYFile("morichang1_mesher.ply", *polygonMesh);
-
-	/*merge = *cloud;
-	merge += *cloud2;
-	merge += *cloud3;
-
-	pcl::io::savePLYFile("merge_cloud.ply", merge);*/
-
-	//pcl::visualization::CloudViewer viewer("Cloud Viewer");
 	pcl::visualization::PCLVisualizer view("MeshMovie");
 
-	//view.registerMouseCallback();
 	view.initCameraParameters();
-	//view.setFullScreen(true);
 	view.setBackgroundColor(0, 0, 0);
 	view.setCameraPosition(-1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0);
-	view.addPolygonMesh(*polygonMesh);
-	//view.addTextureMesh(*textureMesh, "20151203T222732_color");
-	while (key != 'q' /*|| !view.wasStopped()*/){
-		//viewer.showCloud(polygonMesh); //テングンちゃんの表示
+	pcl::io::loadPLYFile(name + std::to_string(i) + ".ply", *polygonMesh);
+
+	while (true){
+		view.addPolygonMesh(*polygonMesh, name + std::to_string(i));
 		view.spin();
 		key = cv::waitKey(1); //ウィンドウの更新
+
+		if (key == 'q'){
+			exit(1);
+		}
+		else if (key == 'n'){
+			++i;
+			polygonMesh->polygons.clear();
+			view.removePolygonMesh(name + std::to_string(i));
+			pcl::io::loadPLYFile(name + std::to_string(i) + ".ply", *polygonMesh);
+		}
+		++i;
+		polygonMesh->polygons.clear();
+		view.removePolygonMesh(name + std::to_string(i));
+		pcl::io::loadPLYFile(name + std::to_string(i) + ".ply", *polygonMesh);
+		if (i <= 60){
+			i = 1;
+		}
 	}
-	//viewer.wasStopped();
+	
 	view.~PCLVisualizer();
 
 	return 0;
