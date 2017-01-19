f669f58982c281bd02c4e23aafa5541dd48b4e12
森田浩平 <morita.aislab@gmail.com>
2017-01-19 21:07:20 +0900

    Depth Imageを点群に変換する部分の処理を書き換えた(型とか)．点群のフィルタリングもここで行なうようにした．

--- PointCloudStudy/PointCloudStudy/FileConvert.cpp
+++ PointCloudStudy/PointCloudStudy/FileConvert.cpp
@@ -1,29 +1,31 @@
 #include "FileConvert.h"
 
-void FileConvert::convPCD2PLY(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud){
+void FileConvert::convPCD2PLY(std::string filename, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud){
 	pcl::PCDReader reader;
+	pcl::PLYWriter writer;
 	std::stringstream FileName;
 	
-	reader.read<pcl::PointXYZRGB>(filename + ".pcd", *cloud);
+	reader.read<pcl::PointXYZ>(filename + ".pcd", *cloud);
 
-	//// PCDファイル形式からPLYファイル形式に変換
+	// PCDファイル形式からPLYファイル形式に変換
 	FileName << filename << ".ply";
 	std::cout << FileName.str() << std::endl;
-	pcl::io::savePLYFileBinary(FileName.str(), *cloud);
+	writer.write<pcl::PointXYZ>(FileName.str(), *cloud, false, false);
 
 	return ;
 }
 
-void FileConvert::convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud){
+void FileConvert::convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud){
 	pcl::PLYReader reader;
+	pcl::PCDWriter writer;
 	std::stringstream FileName;
 	
-	reader.read<pcl::PointXYZRGB>(filename + ".ply", *cloud);
+	reader.read<pcl::PointXYZ>(filename + ".ply", *cloud);
 
-	//// PLYファイル形式からPCDファイル形式に変換
+	// PLYファイル形式からPCDファイル形式に変換
 	FileName << filename << ".pcd";
 	std::cout << FileName.str() << std::endl;
-	pcl::io::savePCDFileBinary(FileName.str(), *cloud);
+	writer.write<pcl::PointXYZ>(FileName.str(), *cloud, false);
 
 	return ;
 }
@@ -32,22 +34,32 @@ void FileConvert::convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXY
 void FileConvert::convPNG2PointCloud(std::string file_path, cv::Point2d param_fl, cv::Point2d param_pp){
 	cv::Mat depthmap = cv::imread(file_path + "depth.png", -1);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointXYZ point;
+	pcl::PLYWriter ply_writer;
 
-	for (int v = 0; v < depthmap.rows; v++){
-		for (int u = 0; u < depthmap.cols; u++){
-			pcl::PointXYZ point;
+	// Depth Imageから点群を生成
+	for (int v = 0; v < depthmap.rows; ++v){
+		for (int u = 0; u < depthmap.cols; ++u){
+			// ミリメートルをメートルに変換
+			point.z = depthmap.at<UINT16>(v, u) / 1000.0;
+			point.x = ((double)u - param_pp.x) * point.z / param_fl.x;
+			point.y = ((double)v - param_pp.y) * point.z / param_fl.y;
 
-			point.z = depthmap.at<UINT16>(v, u);
-			point.x = (u - param_pp.x) * point.z / param_fl.x;
-			point.y = (v - param_pp.y) * point.z / param_fl.y;
-
-			if ((point.x != 0.0) && (point.y != 0.0) && (point.z != 0)){
+			if (point.z != 0.00) {
 				pointcloud->push_back(point);
 			}
 		}
 	}
 
-	pcl::io::savePLYFile(file_path + "pointcloud.ply", *pointcloud);
+	filter.Remove_outliers(pointcloud, pointcloud);
+
+	// カメラの原点を追加
+	point.x = 0.00;
+	point.y = 0.00;
+	point.z = 0.00;
+	pointcloud->push_back(point);
+
+	ply_writer.write<pcl::PointXYZ>(file_path + "pointcloud.ply", *pointcloud, false, false);
 	pointcloud->clear();
 	depthmap.release();
 
--- PointCloudStudy/PointCloudStudy/FileConvert.h
+++ PointCloudStudy/PointCloudStudy/FileConvert.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "stdafx.h"
+#include "CloudFilter.h"
 
 
 class FileConvert{
@@ -9,8 +10,10 @@ private:
 
 public:
 	
-	void convPCD2PLY(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud);
-	void convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud);
+	CloudFilter filter;
+	
+	void convPCD2PLY(std::string filename, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud);
+	void convPLY2PCD(std::string filename, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud);
 	void convPNG2PointCloud(std::string file_path, cv::Point2d param_fl, cv::Point2d param_pp);
 	                      
 };
